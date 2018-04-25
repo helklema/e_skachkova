0557b4c29a06e280b66a5b62660be1c03149f146
helklema <lena.skachkova.96@gmail.com>
2018-04-25 21:32:59 +0300

    New methods

---
+++ new.py
@@ -0,0 +1,85 @@
+# USAGE 
+# python image_diff.py --first images/original_01.png --second images/modified_01.png
+
+# import the necessary packages
+from skimage.measure import compare_ssim
+import argparse
+import imutils
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--first", required=True,
+	help="first input image")
+ap.add_argument("-s", "--second", required=True,
+	help="second")
+args = vars(ap.parse_args())
+
+# load the two input images
+imageA = cv2.imread(args["first"])
+imageB = cv2.imread(args["second"])
+
+imageA2 = imageA.copy()
+template = imageB.copy()
+w, h = template.shape[::-1]
+
+# All the 6 methods for comparison in a list
+methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+		   'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+for meth in methods:
+	img = imageA2.copy()
+	method = eval(meth)
+	# Apply template Matching
+	res = cv2.matchTemplate(img,template,method)
+	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+	# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+	if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+		top_left = min_loc
+	else:
+		top_left = max_loc
+	bottom_right = (top_left[0] + w, top_left[1] + h)
+	cv2.rectangle(img,top_left, bottom_right, 255, 2)
+	plt.subplot(121),plt.imshow(res,cmap = 'gray')
+	plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+	plt.subplot(122),plt.imshow(img,cmap = 'gray')
+	plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+	plt.suptitle(meth)
+	plt.show()
+
+# convert the images to grayscale
+grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+
+# compute the Structural Similarity Index (SSIM) between the two
+# images, ensuring that the difference image is returned
+(score, diff) = compare_ssim(grayA, grayB, full=True)
+diff = (diff * 255).astype("uint8")
+print("SSIM: {}".format(score))
+
+# threshold the difference image, followed by finding contours to
+# obtain the regions of the two input images that differ
+thresh = cv2.threshold(diff, 0, 255,
+	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+
+
+# loop over the contours
+for c in cnts:
+	# compute the bounding box of the contour and then draw the
+	# bounding box on both input images to represent where the two
+	# images differ
+	(x, y, w, h) = cv2.boundingRect(c)
+	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+# show the output images
+cv2.imshow("Original", imageA)
+cv2.imshow("Modified", imageB)
+cv2.imshow("Diff", diff)
+cv2.imshow("Thresh", thresh)
+cv2.waitKey(0)
